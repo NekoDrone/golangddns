935429442cdbbf512eae4b2b3e6a554b95e3f033
Sylfr <sylfr.serenity.tan@gmail.com>
2023-08-05 22:46:57 +0800

    Added core functionality. API Endpoint NYI.

--- src/main.go
+++ src/main.go
@@ -1,7 +1,107 @@
 package main
 
-import "fmt"
+import (
+	"encoding/json"
+	"io"
+	"log"
+	"net/http"
+	"os"
+)
+
+const ipFile string = "lastIp"
 
 func main() {
-	fmt.Println("Hello, Sylfr.")
+	log.Println("Starting DDNS Service.")
+	var lastIp string
+
+	if ipFileExists() {
+		lastIp = readLastIpFromFile()
+	} else {
+		createLastIpFile()
+	}
+
+	log.Println("\nCurrent Outgoing IP:")
+	var currIp string = getCurrentOutgoingIpAsString()
+	log.Println(currIp)
+
+	if currIp != lastIp {
+		writeIpToFile(currIp)
+		updateIpOnExternalDnsRecord(currIp)
+		log.Println("IP Address updated on external DNS site, and on local machine.")
+	} else {
+		log.Println("No IP changes have occurred in the last minute.")
+	}
+}
+
+func getCurrentOutgoingIpAsString() string {
+	req := requestApiToGetIpData("http://ip-api.com/json/")
+	defer (*req).Body.Close()
+	data := convertResponseBodyToByteArray(req)
+	return unmarshalByteArrayToIpString(data)
+}
+
+func requestApiToGetIpData(url string) *http.Response {
+	req, err := http.Get(url)
+	if err != nil {
+		log.Fatal("Failed to get request from external API.\n", err)
+	}
+	return req
+}
+
+func convertResponseBodyToByteArray(responsePointer *http.Response) []byte {
+
+	response := *responsePointer
+	/*
+		http.Get returns a pointer to the response, so we resolve it here.
+		Technically, accessing a field or property on a pointer automatically
+		resolves the pointer, but that's insane behaviour and I'm resolving it for my own sanity.
+	*/
+	data, err := io.ReadAll(response.Body)
+	if err != nil {
+		log.Fatal("Response body converted to invalid array.", err)
+	}
+	return data
+}
+
+func unmarshalByteArrayToIpString(byteArray []byte) string {
+	var ip IP
+	json.Unmarshal(byteArray, &ip)
+	return ip.Query
+}
+
+type IP struct {
+	Query string
+}
+
+func readLastIpFromFile() string {
+	ipFile := openFile(ipFile)
+	return string(ipFile[:])
+}
+
+func openFile(filename string) []byte {
+	fileData, err := os.ReadFile(filename)
+	if err != nil {
+		log.Fatal("Could not open file 'lastIP'. Please check access rights.")
+	}
+	return fileData
+}
+
+func createLastIpFile() {
+	os.Create(ipFile)
+}
+
+func ipFileExists() bool {
+	_, err := os.Stat(ipFile)
+	return err == nil
+}
+
+func writeIpToFile(ip string) {
+	data := make([]byte, len(ip))
+	os.WriteFile(ipFile, data, 0666)
+}
+
+func updateIpOnExternalDnsRecord(ip string) {
+	log.Println("This is where I would have written the API request.")
+	log.Println("The outgoing update would have contained this IP:")
+	log.Println(ip)
 }
